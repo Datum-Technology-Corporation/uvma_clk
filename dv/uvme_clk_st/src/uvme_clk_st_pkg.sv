0849500bb23f09fa2ad229cd4382f18b164db4c3
datum-dpoulin <dpoulin@datumtc.ca>
2022-03-31 16:40:01 -0400

    Updating macro filenames

--- dv/uvme_clk_st/src/uvme_clk_st_pkg.sv
+++ dv/uvme_clk_st/src/uvme_clk_st_pkg.sv
@@ -16,11 +16,11 @@
 
 // Pre-processor macros
 `include "uvm_macros.svh"
-`include "uvml_macros.sv"
-`include "uvml_logs_macros.sv"
-`include "uvml_sb_macros.sv"
-`include "uvma_clk_macros.sv"
-`include "uvme_clk_st_macros.sv"
+`include "uvml_macros.svh"
+`include "uvml_logs_macros.svh"
+`include "uvml_sb_macros.svh"
+`include "uvma_clk_macros.svh"
+`include "uvme_clk_st_macros.svh"
 
 // Interface(s)
 
@@ -30,31 +30,31 @@
  * UVM Agent.
  */
 package uvme_clk_st_pkg;
-   
+
    import uvm_pkg      ::*;
    import uvml_pkg     ::*;
    import uvml_logs_pkg::*;
    import uvml_sb_pkg  ::*;
    import uvma_clk_pkg ::*;
-   
+
    // Constants / Structs / Enums
    `include "uvme_clk_st_tdefs.sv"
    `include "uvme_clk_st_constants.sv"
-   
+
    // Objects
    `include "uvme_clk_st_cfg.sv"
    `include "uvme_clk_st_cntxt.sv"
-   
+
    // Environment components
    `include "uvme_clk_st_cov_model.sv"
    `include "uvme_clk_st_prd.sv"
    `include "uvme_clk_st_vsqr.sv"
    `include "uvme_clk_st_env.sv"
-   
+
    // Sequences
    `include "uvme_clk_st_base_vseq.sv"
    `include "uvme_clk_st_start_and_stop_vseq.sv"
-   
+
 endpackage : uvme_clk_st_pkg
 
 
--- dv/uvmt_clk_st/src/uvmt_clk_st_pkg.sv
+++ dv/uvmt_clk_st/src/uvmt_clk_st_pkg.sv
@@ -16,12 +16,12 @@
 
 // Pre-processor macros
 `include "uvm_macros.svh"
-`include "uvml_macros.sv"
-`include "uvml_logs_macros.sv"
-`include "uvml_sb_macros.sv"
-`include "uvma_clk_macros.sv"
-`include "uvme_clk_st_macros.sv"
-`include "uvmt_clk_st_macros.sv"
+`include "uvml_macros.svh"
+`include "uvml_logs_macros.svh"
+`include "uvml_sb_macros.svh"
+`include "uvma_clk_macros.svh"
+`include "uvme_clk_st_macros.svh"
+`include "uvmt_clk_st_macros.svh"
 
 // Time units and precision for this test bench
 timeunit       1ns;
@@ -32,26 +32,26 @@ timeprecision  1ps;
  * Encapsulates all the types and test cases for self-testing a Clock UVM Agent.
  */
 package uvmt_clk_st_pkg;
-   
+
    import uvm_pkg        ::*;
    import uvml_pkg       ::*;
    import uvml_logs_pkg  ::*;
    import uvml_sb_pkg    ::*;
    import uvma_clk_pkg   ::*;
    import uvme_clk_st_pkg::*;
-   
+
    // Constants / Structs / Enums
    `include "uvmt_clk_st_tdefs.sv"
    `include "uvmt_clk_st_constants.sv"
-   
+
    // Sequences
-   
+
    // Base test
    `include "uvmt_clk_st_test_cfg.sv"
    `include "uvmt_clk_st_base_test.sv"
    `include "uvmt_clk_st_active_test.sv"
    `include "uvmt_clk_st_monitoring_test.sv"
-   
+
 endpackage : uvmt_clk_st_pkg
 
 

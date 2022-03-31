4fd508be38a9ca680f276d584fc43751ad410eaa
datum-dpoulin <dpoulin@datumtc.ca>
2022-03-31 17:07:11 -0400

    fix for macro filename change

--- dv/uvma_clk/src/uvma_clk_pkg.sv
+++ dv/uvma_clk/src/uvma_clk_pkg.sv
@@ -16,9 +16,9 @@
 
 // Pre-processor macros
 `include "uvm_macros.svh"
-`include "uvml_macros.sv"
-`include "uvml_logs_macros.sv"
-`include "uvma_clk_macros.sv"
+`include "uvml_macros.svh"
+`include "uvml_logs_macros.svh"
+`include "uvma_clk_macros.svh"
 
 // Interface(s)
 `include "uvma_clk_if.sv"
@@ -28,35 +28,35 @@
  * Encapsulates all the types needed for the Moore.io Clocking UVM agent.
  */
 package uvma_clk_pkg;
-   
+
    import uvm_pkg      ::*;
    import uvml_pkg     ::*;
    import uvml_logs_pkg::*;
-   
+
    // Constants / Structs / Enums
    `include "uvma_clk_tdefs.sv"
    `include "uvma_clk_constants.sv"
-   
+
    // Objects
    `include "uvma_clk_cfg.sv"
    `include "uvma_clk_cntxt.sv"
-   
+
    // High-level transactions
    `include "uvma_clk_mon_trn.sv"
    `include "uvma_clk_mon_trn_logger.sv"
    `include "uvma_clk_seq_item.sv"
    `include "uvma_clk_seq_item_logger.sv"
-   
+
    // Agent components
    `include "uvma_clk_cov_model.sv"
    `include "uvma_clk_drv.sv"
    `include "uvma_clk_mon.sv"
    `include "uvma_clk_sqr.sv"
    `include "uvma_clk_agent.sv"
-   
+
    // Sequences
    `include "uvma_clk_base_seq.sv"
-   
+
 endpackage : uvma_clk_pkg
 
 
